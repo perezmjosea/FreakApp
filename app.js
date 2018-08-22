8fedb72fdeac6191e521635822baacd07ea2c862
perezm.josea <perezm.josea@gmail.com>
2018-08-23 01:27:56 +0200

    Main Views

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-// var indexRouter = require("./routes/index");
-// var usersRouter = require("./routes/users");
-var appRouter = require("./routes/app-routes");
+// const indexRouter = require("./routes/index");
+// const usersRouter = require("./routes/users");
+const appRouter = require("./routes/app-routes");
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/bower_components", express.static(__dirname + "/bower_components"));
 
 // app.use("/", indexRouter);
 // app.use("/users", usersRouter);
--- routes/app-routes.js
+++ routes/app-routes.js
@@ -1,5 +1,16 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+const apiURL = "https://api.themoviedb.org/3/";
+
+// const movies = axios
+//   .get(apiURL + "discover/movie", {
+//     params: {
+//       api_key: process.env.TMDB_API_KEY
+//     }
+//   })
+//   .catch(e => res.status(500).send("error"));
 
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
@@ -9,8 +20,8 @@ router.get("/home", function(req, res, next) {
   res.render("home", { title: "Freak App" });
 });
 
-router.get("/users", function(req, res, next) {
-  res.send("respond with a resource");
+router.get("/movies", function(req, res, next) {
+  res.render("movies", { title: "Freak App | Movies" });
 });
 
 module.exports = router;
